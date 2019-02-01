2aea5182052a86273da66b92b1cebddefaa58efa
khatryshikha <shikhakhatry2612@gmail.com>
2019-02-01 16:08:27 +0530

    stop button

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name ='home'),
     url(r'^upload', views.check_new_entry, name ='upload'),
+    url(r'^stop', views.stop_csv, name ='stop'),
 ]
--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from django.conf import settings
 from .database import dbs as db
 
 # Create your views here.
-
+flag = False
 def home(request):
     return render(request,'upload.html')
 
@@ -23,17 +23,28 @@ def check_new_entry(request):
         count = 0
         data = []
         head_list = []
+        global flag
+        flag = False
         iterator = tqdm(spamreader,total=row_count)
         for row in iterator:
             time.sleep(0.1)
+            if(flag == True):
+                break
             if ( count == 0 ):
                 head_list = row
                 count = count + 1
             else: 
                 data.append(dict(zip(head_list, row)))
                 count= count +1
-        db.test.insert(data)
+        if(flag == False):
+            db.test.insert(data)
         return JsonResponse( {'code': '1',
-							'Message':'Succesfully added data to database',
-							'status': 'success'})
+						'Message':'Succesfully added data to database',
+						'status': 'success'})
 
+def stop_csv(request):
+    global flag
+    flag = True
+    return JsonResponse( {'code': '0',
+							'Message':'Fail to process/Force stopped',
+							'status': 'fail'})
